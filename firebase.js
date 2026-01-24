8fb5451c7aa1f261eb4a8d083b973802be923592
William Maclean <wmacl006@gmail.com>
2026-01-24 10:52:20 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,14 +6,14 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-s
 const firebaseConfig = {
   apiKey: "AIzaSyDAScHIxTwrXQEVCnEYxizNPSRKiuYsqqA",
   authDomain: "teampdf-7ec12.firebaseapp.com",
-  databaseURL: "https://teampdf-7ec12-default-rtdb.firebaseio.com",
   projectId: "teampdf-7ec12",
   storageBucket: "teampdf-7ec12.firebasestorage.app",
   messagingSenderId: "307072046237",
   appId: "1:307072046237:web:d1f44f115fdf199b5a7074"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
